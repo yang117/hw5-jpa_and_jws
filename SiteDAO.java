c5a33d68c3b69b1e523a0018acfaa5a9c0c51a70
yang117 <caoy117@gmail.com>
2015-04-14 21:57:52 -0400

    Update SiteDAO.java

--- SiteDAO.java
+++ SiteDAO.java
@@ -126,64 +126,64 @@ public class SiteDao {
 		
 	}
 	
-//	public void exportSiteListToXmlFile(SiteList siteList, String xmlFileName){
-//		File xmlFile = new File(xmlFileName);
-//		try {
-//			JAXBContext jaxb = JAXBContext.newInstance(SiteList.class);
-//			Marshaller marshaller = jaxb.createMarshaller();
-//			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//			marshaller.marshal(siteList, xmlFile);
-//		} catch (JAXBException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public void convertXmlFileToOutputFile(
-//			String inputXmlFileName, 
-//			String outputXmlFileName, 
-//			String xsltFileName)
-//	{
-//		File inputXmlFile = new File(inputXmlFileName);
-//		File outputXmlFile = new File(outputXmlFileName);
-//		File xsltFile = new File(xsltFileName);
-//		
-//		StreamSource source = new StreamSource(inputXmlFile);
-//		StreamSource xslt   = new StreamSource(xsltFile);
-//		StreamResult output = new StreamResult(outputXmlFile);
-//		
-//		TransformerFactory factory = TransformerFactory.newInstance();
-//		try {
-//			Transformer transformer = factory.newTransformer(xslt);
-//			transformer.transform(source, output);
-//		} catch (TransformerConfigurationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (TransformerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		
-//		
-//	}
-//
-//	public static void main(String[] args) {
-//		SiteDao dao = new SiteDao();
-//		
-//		List<Site> sites = dao.findAllSites();
-//		for(Site st:sites){
+	public void exportSiteListToXmlFile(SiteList siteList, String xmlFileName){
+		File xmlFile = new File(xmlFileName);
+		try {
+			JAXBContext jaxb = JAXBContext.newInstance(SiteList.class);
+			Marshaller marshaller = jaxb.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.marshal(siteList, xmlFile);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void convertXmlFileToOutputFile(
+			String inputXmlFileName, 
+			String outputXmlFileName, 
+			String xsltFileName)
+	{
+		File inputXmlFile = new File(inputXmlFileName);
+		File outputXmlFile = new File(outputXmlFileName);
+		File xsltFile = new File(xsltFileName);
+		
+		StreamSource source = new StreamSource(inputXmlFile);
+		StreamSource xslt   = new StreamSource(xsltFile);
+		StreamResult output = new StreamResult(outputXmlFile);
+		
+		TransformerFactory factory = TransformerFactory.newInstance();
+		try {
+			Transformer transformer = factory.newTransformer(xslt);
+			transformer.transform(source, output);
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		SiteDao dao = new SiteDao();
+		
+		List<Site> sites = dao.findAllSites();
+		for(Site st:sites){
 //			System.out.println(st.getName());	
-//		}
-//		
-//	SiteList sitelist = new SiteList();
-//	sitelist.setSites(sites);
-//	dao.exportSiteListToXmlFile(sitelist, "xml/sites2.xml");
-//		
+		}
+		
+	SiteList sitelist = new SiteList();
+	sitelist.setSites(sites);
+	dao.exportSiteListToXmlFile(sitelist, "xml/sites2.xml");
+		
 //    	dao.convertXmlFileToOutputFile("xml/sites.xml", "xml/sites.html","xml/sites2html.xslt");
 //		dao.convertXmlFileToOutputFile("xml/sites.xml", "xml/equipments.html","xml/sites2equipment.xslt");
-//		// TODO Auto-generated method stub
-//
-//	}
+		// TODO Auto-generated method stub
+
+	}
 
 }
